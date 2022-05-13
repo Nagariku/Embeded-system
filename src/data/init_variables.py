3ef5886dbfd44acdb0256ed7ef12f9e68a51157d
Nagariku <osipovgrisha.ru2@gmail.com>
2022-05-13 11:53:51 +0200

    compacting variables

--- src/data/init_variables.py
+++ src/data/init_variables.py
@@ -2,36 +2,28 @@
 ##aka don't touch these or program won't work
 
 #Turtlebot instance
-#tb = Turtlebot() 
+tb = Turtlebot() 
 
 #booleans
 robotRunning = True
 timeTickUpdate_bool = False
 
 #lists
-timeFromStartArray = [0,0]
-DeadReckon_List_theta = [0,0]
-DeadReckon_List_vel = [0,0]
-current_target=[0,0]
+timeFromStartArray = DeadReckon_List_theta = DeadReckon_List_vel = current_target = [0,0]
 
 #Graph plotting CORY
-IMUList = []
-abscissaList = [] # for plotting graph
-ordinateList = [] # for plotting graph
-ordinateList2 = [] # for plotting graph
+IMUList = abscissaList = ordinateList = ordinateList2 = [] # for plotting graph
+
 
 #integers
 globalLoopCounter = 1
-refTickLeft = 0 #was None before
-refTickRight = 0 #was None before
+refTickLeft = refTickRight = 0 #was None before
+
 
 #floats     
-current_x = 0
-current_y = 0
+current_x = current_y =  0
+distance_travelled = angle_total =  0
+forward_velocity = angular_velocity = 0
 theta = 0
-distance_travelled = 0
-angle_total = 0
-forward_velocity = 0
-angular_velocity = 0
 global_velocity_signal = 0
 
